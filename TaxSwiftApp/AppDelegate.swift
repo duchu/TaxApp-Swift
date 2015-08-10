9ebb6c53e8cf96ea6340bfa990b2a56d415bb16d
Ikko Fushiki <ifushiki@yahoo.com>
2015-08-09 22:23:47 -0700

    Initial Commit

---
+++ TaxSwiftApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TaxSwiftApp
+//
+//  Created by IKKO FUSHIKI on 8/9/15.
+//  Copyright (c) 2015 IKKO FUSHIKI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TaxSwiftApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TaxSwiftApp
+//
+//  Created by IKKO FUSHIKI on 8/9/15.
+//  Copyright (c) 2015 IKKO FUSHIKI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
