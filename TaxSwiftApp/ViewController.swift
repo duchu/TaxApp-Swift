2fac9f79dfda417cb383c04af3f3e9bc3fb985df
Ikko Fushiki <ifushiki@yahoo.com>
2015-08-10 14:09:33 -0700

    Images and scorers are added.

--- TaxSwiftApp/ViewController.swift
+++ TaxSwiftApp/ViewController.swift
@@ -10,10 +10,21 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var headerImage: NSImageView!
+    @IBOutlet weak var scrollView: NSScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let util:ResourceUtil = ResourceUtil()
+        headerImage.image = util.getImage("TurboTax W-2 Header", type: "png")
+        let mainView: NSImageView = NSImageView()
+        let pageImage: NSImage = util.getImage("TurboTax W-2 Page-1", type:"png")
+        let imageSize: CGSize = pageImage.size
+        mainView.image = pageImage
+        mainView.setFrameSize(imageSize)
+        scrollView.documentView = mainView
     }
 
     override var representedObject: AnyObject? {
