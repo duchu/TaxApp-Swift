60d2f5fc282856f426155c04e03143995af23ea1
Ikko Fushiki <ifushiki@yahoo.com>
2015-08-18 06:06:36 -0700

    Mouse dragging of the arrow is implemented.

--- TaxSwiftApp/GraphView.swift
+++ TaxSwiftApp/GraphView.swift
@@ -9,16 +9,24 @@
 import Cocoa
 
 class GraphView : NSView {
-    let kMaximumSigma : CGFloat = 8
+    let kMaximumSigma : CGFloat = 8         // Maximum sigma (x-coordinate will go from - kMaxSigma to + kMaxSigma
+    let kBottomMergin : CGFloat = 0.1       // Bottom mergin (relative to the bounding height)
+    let kScaleY : CGFloat = 0.8             // yScale (relative to the bounding height)
     var sigma : CGFloat
+    var arrow : [CGPoint]
+    var isDragging: Bool
 
     override init(frame frameRect: NSRect) {
         self.sigma = 0.0
+        self.arrow = Array<CGPoint>()
+        isDragging = false
         super.init(frame:frameRect)
     }
     
     required init?(coder: NSCoder) {
         self.sigma = 0.0
+        self.arrow = Array<CGPoint>()
+        isDragging = false
         super.init(coder:coder)
     }
     
@@ -41,9 +49,9 @@ class GraphView : NSView {
         cPath.fill()
     }
     
+
     func getXCoordinateFromSigma(sigma: CGFloat) -> CGFloat
     {
-        
         let rect = self.bounds;
         let xRange: CGFloat = 2.0*kMaximumSigma
         let dTick : CGFloat = rect.size.width/xRange
@@ -52,18 +60,45 @@ class GraphView : NSView {
     
         return arrowPosition;
     }
+
+    func getSigmaFromXCoordinate(x: CGFloat) -> CGFloat
+    {
+        var sig: CGFloat = 0.0
+        
+        let rect = self.bounds;
+        let xRange: CGFloat = 2.0*kMaximumSigma
+        let dTick : CGFloat = rect.size.width/xRange
+        
+        sig = x/dTick - kMaximumSigma
+        
+        return sig
+    }
+    
+    func getArrowCoordinate() -> CGPoint
+    {
+        var pt : CGPoint = CGPoint()
+        let rect = self.bounds;
+        let xRange: CGFloat = 2.0*kMaximumSigma
+        let dTick : CGFloat = rect.size.width/xRange
+        let yBottom :CGFloat = rect.size.height*kBottomMergin;
+        
+        pt.x = dTick*(self.sigma + kMaximumSigma)  // 4.0 is a mid range.
+        pt.y = yBottom
+
+        return pt
+    }
     
     // Create arrow path points.
-    func setArraw(inout arrow: [CGPoint], w: CGFloat, h: CGFloat)
+    func setArrawPath(inout arrow1: [CGPoint], w: CGFloat, h: CGFloat)
     {
-        arrow.append(CGPointMake(0.0, 0.0))
-        arrow.append(CGPointMake(w, h))
-        arrow.append(CGPointMake(w/2, h))
-        arrow.append(CGPointMake(arrow[2].x, 3*h))
-        arrow.append(CGPointMake(0.0 - arrow[3].x, arrow[3].y))
-        arrow.append(CGPointMake(arrow[4].x, arrow[1].y))
-        arrow.append(CGPointMake(0.0 - arrow[1].x, arrow[1].y))
-        arrow.append(CGPointMake(arrow[0].x, arrow[0].y))
+        arrow1.append(CGPointMake(0.0, 0.0))
+        arrow1.append(CGPointMake(w, h))
+        arrow1.append(CGPointMake(w/2, h))
+        arrow1.append(CGPointMake(arrow[2].x, 3*h))
+        arrow1.append(CGPointMake(0.0 - arrow[3].x, arrow[3].y))
+        arrow1.append(CGPointMake(arrow[4].x, arrow[1].y))
+        arrow1.append(CGPointMake(0.0 - arrow[1].x, arrow[1].y))
+        arrow1.append(CGPointMake(arrow[0].x, arrow[0].y))
     }
     
     // This calculate the bounding border from sigma1 to sigma2.
@@ -125,8 +160,8 @@ class GraphView : NSView {
 //        CFRelease(borderPath);
         
         let n = 100;
-        let yFac = rect.size.height*0.8;
-        let yBottom = rect.size.height*0.1;
+        let yFac = rect.size.height*kScaleY;
+        let yBottom = rect.size.height*kBottomMergin;
         let xRange = 2.0*kMaximumSigma;
         let dx = rect.size.width/(CGFloat(n) - 1);
         let middle = 0.5*CGFloat(n);
@@ -181,16 +216,24 @@ class GraphView : NSView {
         
         // Draw an arrow.
         let arrowPosition = self.getXCoordinateFromSigma(self.sigma)
+        let arrowOrig: CGPoint = self.getArrowCoordinate()
         var arrowPath: CGMutablePathRef = CGPathCreateMutable();
         
-        var arrow: [CGPoint] = Array<CGPoint>()
+//        var arrow: [CGPoint] = Array<CGPoint>()
         let w = rect.size.width*0.02;
         let h = rect.size.height*0.05;
-        setArraw(&arrow, w:w, h:h)
-        CGPathMoveToPoint(arrowPath, nil, arrow[0].x + arrowPosition, arrow[0].y + yBottom);
+        
+        // Set the arrow array is it is not set.
+        if (self.arrow.count == 0) {
+            setArrawPath(&self.arrow, w:w, h:h)
+        }
+        
+//        CGPathMoveToPoint(arrowPath, nil, self.arrow[0].x + arrowPosition, self.arrow[0].y + yBottom);
+        CGPathMoveToPoint(arrowPath, nil, self.arrow[0].x + arrowOrig.x, self.arrow[0].y + arrowOrig.y);
         
         for (var i: Int = 1; i < 8; i++) {
-            CGPathAddLineToPoint(arrowPath, nil, arrow[i].x + arrowPosition, arrow[i].y + yBottom);
+//            CGPathAddLineToPoint(arrowPath, nil, arrow[i].x + arrowPosition, arrow[i].y + yBottom);
+            CGPathAddLineToPoint(arrowPath, nil, arrow[i].x + arrowOrig.x, arrow[i].y + arrowOrig.y);
         }
         
         CGContextBeginPath(ctx);
@@ -202,5 +245,46 @@ class GraphView : NSView {
         CGContextFillPath(ctx);
         CGContextStrokePath(ctx);
 //        CFRelease(arrowPath);
+    }
+    
+    override func mouseDown(theEvent: NSEvent) {
+        //
+        let arrowPt = self.getArrowCoordinate()
+        let viewPt = self.convertPoint(theEvent.locationInWindow, fromView: nil)
+        var testRect = CGRect()
+        let delta: CGFloat = 20.0
+        testRect.origin.x = viewPt.x - delta
+        testRect.origin.y = viewPt.y - delta
+        testRect.size.width = 2*delta
+        testRect.size.height = 2*delta
+        
+        // Check if the clicked point is near the arrow.
+        if  (testRect.contains(arrowPt)) {
+            self.isDragging = true
+            self.setNeedsDisplayInRect(self.bounds)
+        }
+    }
+    
+    override func mouseDragged(theEvent: NSEvent) {
+        // Move the arrow if it is being dragged.
+        if (self.isDragging) {
+            let viewPoint = self.convertPoint(theEvent.locationInWindow, fromView: nil)
+            self.sigma = self.getSigmaFromXCoordinate(viewPoint.x)
+            self.setNeedsDisplayInRect(self.bounds)
+        }
+    }
+    
+    override func mouseMoved(theEvent: NSEvent) {
+        //
+    }
+    
+    override func mouseUp(theEvent: NSEvent) {
+        // Move the arrow to the final location and finish the dragging.
+        if (self.isDragging) {
+            let viewPoint = self.convertPoint(theEvent.locationInWindow, fromView: nil)
+            self.sigma = self.getSigmaFromXCoordinate(viewPoint.x)
+            self.setNeedsDisplayInRect(self.bounds)
+            self.isDragging = false
+        }
     }
 }
